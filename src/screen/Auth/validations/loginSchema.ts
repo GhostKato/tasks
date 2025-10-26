e87cb3bc06b082617a09a82abe1818707f680127
GhostKato <osadchenko.maksym@gmail.com>
2025-10-26 11:00:46 +0200

    fixed auth schema

--- src/screen/Auth/validations/loginSchema.ts
+++ src/screen/Auth/validations/loginSchema.ts
@@ -4,9 +4,12 @@ export const LoginSchema = Yup.object().shape({
   email: Yup
     .string()
     .email('Invalid email address')
+    .min(11, 'Must be at least 11 characters long')
+    .max(30, 'Must be at least 30 characters')
     .required('Email is required'),
   password: Yup
     .string()
-    .min(8, 'Password must be at least 8 characters')
+    .min(5, 'Must be at least 5 characters long')
+    .max(20, 'Must be at least 20 characters')
     .required('Password is required'),
 });
--- src/screen/Auth/validations/registrationSchema.ts
+++ src/screen/Auth/validations/registrationSchema.ts
@@ -2,14 +2,18 @@ import * as Yup from 'yup';
 
 export const RegistrationSchema = () =>
   Yup.object({
-    email: Yup.string().email('Invalid Email').required('Email is empty'),
+    email: Yup.string()
+      .email('Invalid Email')
+      .min(11, 'Must be at least 11 characters long')
+      .max(30, 'Must be at least 30 characters')
+      .required('Email is empty'),
     password: Yup.string()
       .matches(new RegExp(/^.{8,100}$/), {
-        message: 'Password mast be mor then 8 symbols',
+        message: 'Password must be mor then 8 symbols',
       })
       .required('Password is empty'),
     confirmPassword: Yup.string().oneOf(
-      [Yup.ref('password'), null],
+      [Yup.ref('password')],
       'Passwords must match',
     ),
   });
